d08925c8a42033b7ef70a7508e47622725df2c38
valmur <pradovalmur@outlook.com>
2024-05-09 19:15:16 +0100

    stil here, preparations to datavault modeling

--- workspace/notebooks/datavault_utils.py
+++ workspace/notebooks/datavault_utils.py
@@ -1,36 +1,15 @@
-# Databricks notebook source
 import dlt
+from pyspark.sql.functions import col
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import to_date, date_format
 
-
-def check_and_create_mount(mount_point, source):
-
-    
-
-    try:
-        # Check if the mount point already exists
-        dbutils.fs.ls(mount_point)
-        print(f"Mount point '{mount_point}' already exists.")
-        return True
-    except Exception as e:
-        # Mount point doesn't exist, create it
-        print(f"Mount point '{mount_point}' does not exist. Creating...")
-        try:
-            dbutils.fs.mount(source, mount_point, extra_configs=extra_configs)
-            print(f"Mount point '{mount_point}' created successfully.")
-            return True
-        except Exception as e:
-            print(f"Failed to create mount point '{mount_point}': {str(e)}")
-            return False
-
-storageAccountName = "charlesdatabricksadlsno"
-blobContainerName = "aaa"
-mountPoint = "/mnt/files/"
+mountPoint = "/mnt/data"
 if not any(mount.mountPoint == mountPoint for mount in dbutils.fs.mounts()):
   try:
     dbutils.fs.mount(
-      source = "wasbs://{}@{}.blob.core.windows.net".format(blobContainerName, storageAccountName),
-      mount_point = mountPoint,
-      extra_configs = {"fs.azure.account.key.cmstorageacc1251.blob.core.windows.net":dbutils.secrets.get(scope = "lakehouse", key = "secret-sauce")}
+        source = "wasbs://cmdatalake1251@cmstorageacc1251.blob.core.windows.net",
+        mount_point = "/mnt/data",
+        extra_configs = {"fs.azure.account.key.cmstorageacc1251.blob.core.windows.net":dbutils.secrets.get(scope = "lakehouse", key = "secret-sauce")}
     )
     print("mount succeeded!")
   except Exception as e:
@@ -40,11 +19,18 @@ if not any(mount.mountPoint == mountPoint for mount in dbutils.fs.mounts()):
 def stg_investidors():
     try:
     
-        return (
-            spark.readStream.format("cloudFiles")
-            .option("cloudFiles.format", "json")
-            .load("/mnt/files/investidors/")
-        )
+        df = spark.readStream.format("cloudFiles").option("cloudFiles.format", "json").load("/mnt/data/stg-files/investidors/")
+        
+        new_column_names = [col(old_column).alias(old_column.replace(' ', '_').lower()) for old_column in df.columns]
+        
+        df = df.select(*new_column_names)
+
+        date_columns = ['data_de_adesao']
+
+        for column in date_columns:
+            df = df.withColumn(column, date_format(to_date(column, 'dd/MM/yyyy'), 'yyyy-MM-dd'))
+
+        return df
 
     except Exception as e:
         print(f"Error {e}")
@@ -53,11 +39,18 @@ def stg_investidors():
 def stg_transaction():
     try:
     
-        return (
-            spark.readStream.format("cloudFiles")
-            .option("cloudFiles.format", "json")
-            .load("/mnt/files/transaction/")
-        )
+        df = spark.readStream.format("cloudFiles").option("cloudFiles.format", "json").load("/mnt/data/stg-files/transaction/")
+
+        new_column_names = [col(old_column).alias(old_column.replace(' ', '_').lower()) for old_column in df.columns]
+        
+        df = df.select(*new_column_names)
+
+        date_columns = ['data_da_operacao', 'vencimento_do_titulo']
+
+        for column in date_columns:
+            df = df.withColumn(column, date_format(to_date(column, 'dd/MM/yyyy'), 'yyyy-MM-dd'))
+
+        return df
 
     except Exception as e:
         print(f"Error {e}")
@@ -65,17 +58,55 @@ def stg_transaction():
 
 @dlt.view
 def stock():
-    None
+    return spark.sql("""SELECT distinct
+                            monotonically_increasing_id() as stock_id,
+                            concat(t.tipo_titulo,"-", t.vencimento_do_titulo),
+                            t.tipo_titulo,
+                            t.vencimento_do_titulo,
+                            t.valor_do_titulo
+                    FROM LIVE.stg_transaction as t
+                      """)
 
 @dlt.view
 def transaction():
-    None
+    return spark.sql("""
+                    select 
+                        monotonically_increasing_id() as transaction_id,
+                        t.codigo_do_investidor,
+                        t.data_da_operacao,
+                        t.tipo_titulo,
+                        t.vencimento_do_titulo,
+                        concat(t.tipo_Titulo, '-', t.vencimento_do_titulo) as titulo,
+                        t.quantidade ,
+                        t.valor_da_operacao,
+                        t.tipo_da_operacao,
+                        t.canal_da_operacao
+                    from LIVE.stg_transaction as t
+                    """)
 
 @dlt.view
 def investidor():
-    None
+    return spark.sql("""
+                  select
+                    i.codigo_do_investidor,
+                    i.estado_civil,
+                    i.genero,
+                    i.profissao,
+                    i.idade,
+                    i.uf_do_investidor,
+                    i.cidade_do_investidor,
+                    i.pais_do_investidor
+                  from LIVE.stg_investidors as i
+                  """)
+    
 
 @dlt.view
 def account():
-    None
-
+    return spark.sql("""
+                     select 
+                        i.codigo_do_investidor,
+                        i.data_de_adesao,
+                        i.situacao_da_conta,
+                        i.operou_12_meses 
+                     from LIVE.stg_investidors as i
+                     """)
